9abc25544d3617c02a5d5e834d7abb9d8e12574f
Minkyung Jeong <s2335@e-mirim.hs.kr>
2025-08-29 10:42:35 +0900

    내역 보기 창 디자인 구현

---
+++ FormHistory.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace DreamTeller3112
+{
+    partial class FormHistory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lbHistory = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(12, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(328, 52);
+            this.btnRefresh.TabIndex = 0;
+            this.btnRefresh.Text = "내역 다시 불러오기";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // lbHistory
+            // 
+            this.lbHistory.FormattingEnabled = true;
+            this.lbHistory.ItemHeight = 12;
+            this.lbHistory.Location = new System.Drawing.Point(12, 85);
+            this.lbHistory.Name = "lbHistory";
+            this.lbHistory.Size = new System.Drawing.Size(327, 376);
+            this.lbHistory.TabIndex = 1;
+            this.lbHistory.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // FormHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 484);
+            this.Controls.Add(this.lbHistory);
+            this.Controls.Add(this.btnRefresh);
+            this.Name = "FormHistory";
+            this.Text = "내역 보기";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ListBox lbHistory;
+    }
+}
