cc9a164f8bdd37aaeb28572da37fed49b2ba860d
Minkyung Jeong <s2335@e-mirim.hs.kr>
2025-08-29 12:16:21 +0900

    내역 불러오기 로직 구현

--- FormHistory.cs
+++ FormHistory.cs
@@ -1,25 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DreamTeller3112
 {
     public partial class FormHistory : Form
     {
-        public FormHistory()
+        List<string> history;
+        Form1 form1;
+
+        public FormHistory(Form1 form)
         {
+            this.form1 = form;
             InitializeComponent();
+            UpdateHistory();
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void LoadHistory()
         {
-
+            try
+            {
+                string filename = "history.csv";
+                history = File.ReadAllLines(filename).ToList();
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show($"파일을 불러올 수 없습니다.\n{ex.Message}", "파일 없음");
+                history = new List<string>();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"알 수 없는 오류가 발생했습니다.\n{ex.Message}", "알 수 없는 오류",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                history = new List<string>();
+            }
         }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            UpdateHistory();
+        }
+
+        public void UpdateHistory()
+        {
+            LoadHistory();
+            lbHistory.Items.Clear();
+            lbHistory.Items.AddRange(history.ToArray());
+        }
+
+        private void lbHistory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbHistory.SelectedIndex >= 0)
+            {
+                string message = history[lbHistory.SelectedIndex];
+                form1.LoadHistory(message);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) { }
     }
 }
